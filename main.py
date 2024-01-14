3e61b5642941f27563d1b47ecb019b65b8b2097b
JusticeTomato <43867062+JusticeTomato@users.noreply.github.com>
2024-01-14 17:25:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+    
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
