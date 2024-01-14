c007535da9d824f124002f7256028c94510d9933
JusticeTomato <43867062+JusticeTomato@users.noreply.github.com>
2024-01-14 23:24:05 +0900

    Update main.js

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -18,12 +18,12 @@
 			xsmall:   [ null,      '480px'  ]
 		});
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	// // Play initial animations on page load.
+	// 	$window.on('load', function() {
+	// 		window.setTimeout(function() {
+	// 			$body.removeClass('is-preload');
+	// 		}, 100);
+	// 	});
 
 	// Touch mode.
 		if (browser.mobile)
