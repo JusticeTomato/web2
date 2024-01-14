f7c7fd2ef540189d9c7b6ed3b02999ff4dc61c69
JusticeTomato <43867062+JusticeTomato@users.noreply.github.com>
2024-01-15 01:12:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-from fastapi import FastAPI
-import uvicorn
+from fastapi import FastAPI, Depends, HTTPException, Request, Form, Path
+from sqlalchemy import create_engine, Column, Integer, String, Text, MetaData, DateTime, text
+from sqlalchemy.orm import declarative_base, Session
+from sqlalchemy.ext.declarative import DeclarativeMeta
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import os
+cache = {}
+def get_template(template_name: str, context: dict):
+    if template_name in cache:
+        return cache[template_name]
+
+    template = templates.TemplateResponse(template_name, context)
+    cache[template_name] = template
+    return template
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+Base: DeclarativeMeta = declarative_base()
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), index=True)
+    content = Column(Text)
+    created_at = Column(DateTime, server_default=text('CURRENT_TIMESTAMP'))
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def root():
